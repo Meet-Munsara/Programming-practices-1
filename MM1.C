43e9ec9fe153c2ff8ddd7ad871c43b49743cd8e3
meetmunsara459-beep <meetmunsara459@gmail.com>
2026-09-08 22:55:13 +0530

    Add files via upload

---
+++ MM1.C
@@ -0,0 +1,17 @@
+// write a program to input year and find whether your year is leap year or mot
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+
+	clrscr();
+	printf("Hello world");
+
+
+
+	getch();
+}
+
