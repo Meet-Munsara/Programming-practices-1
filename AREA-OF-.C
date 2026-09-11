92f0e063969bccbf75c754ef78096d32fe4ce340
Meet Munsara <meetmunsara459@gmail.com>
2026-09-11 22:50:43 +0530

    Add files via upload

---
+++ AREA-OF-.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	 //area of circle=3.14*r*r
+
+	float area,r;
+	clrscr();
+	printf("Enter the redius of circle:");
+	scanf("%f",&r);
+
+	area=3.14*r*r;
+
+	printf("Area of circle is:%f",area);
+
+
+
+
+
+	getch();
+}
+
+
